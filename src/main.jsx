fd2a986d77f8bcda0b6cdc38fa76a26b4260d918
carloscamacho <carlosalvarez9805@gmail.com>
2022-11-07 17:16:40 -0600

    Create a counter with customHook

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HooksApp } from './HooksApp';
-import { CounterApp } from "./01-useState/CounterApp";
+/* import { HooksApp } from './HooksApp';
+import { CounterApp } from "./01-useState/CounterApp"; */
+
+import { CounterWithCustormHook } from './01-useState/CounterWithCustormHook';
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp />
+    <CounterWithCustormHook />
   </React.StrictMode>
 )
