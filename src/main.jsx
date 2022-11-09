19771e78ed4d7082350ffec98e324b9714313ff1
carloscamacho <carlosalvarez9805@gmail.com>
2022-11-09 13:33:24 -0600

    Create simpleForm with useEffect and dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-/* import { HooksApp } from './HooksApp';
-import { CounterApp } from "./01-useState/CounterApp"; */
 
-import { CounterWithCustormHook } from './01-useState/CounterWithCustormHook';
+// import { HooksApp } from './HooksApp';
+// import { CounterApp } from "./01-useState/CounterApp";
+// import { CounterWithCustormHook } from './01-useState/CounterWithCustormHook';
+import { SimpleForm } from './02-useEffect/SimpleForm'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CounterWithCustormHook />
-  </React.StrictMode>
+  
+    <SimpleForm />
+  
 )
