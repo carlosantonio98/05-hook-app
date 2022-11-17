b33d27fe573c34fb203677ecee253bccb3c05ee2
carloscamacho <carlosalvarez9805@gmail.com>
2022-11-17 15:13:51 -0600

    Create inputFocus with useRef hook

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,14 @@ import ReactDOM from 'react-dom/client'
 // import { CounterWithCustormHook } from './01-useState/CounterWithCustormHook';
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+//import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <MultipleCustomHooks />
+    <FocusScreen />
   
 )
