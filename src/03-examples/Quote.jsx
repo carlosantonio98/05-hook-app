9c0649e6c3cfb8c4f5d2a6796a3ec4057321e162
carloscamacho <carlosalvarez9805@gmail.com>
2022-11-18 16:32:16 -0600

    Create breakingBad Quotes with useLayoutEffect hook

--- src/03-examples/Quote.jsx
+++ src/03-examples/Quote.jsx
@@ -1,8 +1,31 @@
+import { useLayoutEffect, useRef, useState } from "react"
+
 export const Quote = ({ quote, author }) => {
+  
+  const pRef = useRef();
+  const [boxSize, setBoxSize] = useState({
+    width: 0, height: 0
+  });
+
+  useLayoutEffect(() => {
+    // console.log( pRef.current.getBoundingClientRect() );
+    const { width, height } = pRef.current.getBoundingClientRect();  // esto retorna toda la información del tamaño y de la posición del elemento html en un objeto.
+
+    setBoxSize({ width, height });
+
+  }, [quote])
+
   return (
-    <blockquote className="blockquote text-end">
-        <p className="mb-1">{ quote }</p>
+    <>
+      <blockquote 
+        className="blockquote text-end"
+        style={{ display: 'flex' }}
+      >
+        <p ref={ pRef } className="mb-1">{ quote }</p>
         <footer className="blockquote-footer"> { author }</footer>
-    </blockquote>
+      </blockquote>
+
+      <code>{ JSON.stringify(boxSize) }</code>
+    </>
   )
 }
---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,39 @@
+import { useCounter, useFetch } from '../hooks';
+import { LoadingQuote, Quote } from '../03-examples';
+
+export const Layout = () => {
+    const { counter, increment }        = useCounter(1);
+    const { data, isLoading, hasError } = useFetch(`https://www.breakingbadapi.com/api/quotes/${ counter }`);
+
+    // Si intentamos destructurar un false no dara error, ya que author y quote tendran como valor undefined.
+    /*
+        const { a, b } = false;   
+
+        console.log({ a, b }); // { undefined, undefined }
+    */
+    // No podemos desestructurar algo nulo o undefined, la unica manera de desestructurar un nulo o indefinded es haciendo una doble negacion de estos,  si negamos !null esto da true, es decir lo opuesto de vacio es algo que tiene valor y si lo volvemos a negar !!null esto nos dara un false, lo mismo pasa con el undefined, en pocas palabras el !!data es como una condicional, si !!data es true va a ejecutar la siguiente linea y va a regresarnos el siguiente valor que es la data en la posicion 0 "data[0]"
+    const { author, quote } = !!data && data[0];
+
+    // console.log({ data, isLoading, hasError });
+
+    return (
+        <>
+            <h1>BreakingBad Quotes</h1>
+            <hr />
+
+            {/* Es recomendable usar esto si solo son 3 o 4 lineas de código */}
+            {
+                isLoading
+                    ? <LoadingQuote />  // Hay que priorizar trabajar con el principio de responsabilidad unica
+                    : <Quote quote={ quote } author={ author } />
+            }
+            
+            <button className="btn btn-primary"
+                    onClick={ () => increment() }
+                    disabled={ isLoading }>
+                Next quote
+            </button>
+            
+        </>
+    );
+}
