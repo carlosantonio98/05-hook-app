57ebd4490217304b55d018a49dc2c28c0fe7295e
carloscamacho <carlosalvarez9805@gmail.com>
2022-12-21 20:41:00 -0600

    Configure Router in React

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,9 +1,28 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { HomePage, AboutPage, LoginPage } from '.';
 
 export const MainApp = () => {
     return (
         <>
             <h1>Main app</h1>
             <hr />
+
+            <Routes>
+                <Route path='/' element={ <HomePage /> } />
+                <Route path='about' element={ <AboutPage /> } />
+                <Route path='login' element={ <LoginPage /> } />
+
+                {/* a /* se le conoce como while car, la ruta que se muestra a continuación es una tecnica que se utiliza para redirigir al login en caso de que se busque una ruta que no existe
+                <Route path='/*' element={ <LoginPage /> } />
+                */}
+                
+                {/* esto hace lo mismo que lo anterior la diferencia es que este es mas común de encontrar y mayormente lo vamos a trabajar de esta manera, para hacerlo de esta manera vamos a necesitar implemetar el componente Navigate, a este componente  le vamos a especificar en su propiedad "to" hacia que ruta lo queremos redirigir, así como se muestra a continuación */}
+                {/*
+                    Navigate es especial es un componente como otros, solo que este componente apenas se renderize, se termine de implementar y el contexto de nuestros componentes se normalize o ya todos esten dibujados, entoces va hacer la navegacion al componente o al path que nosotros especifiquemos en el to.
+                
+                */}
+                <Route path='/*' element={ <Navigate to="/" /> } />
+            </Routes>
         </>
     )
 }
