1faba476d300aa0668a3ab8475696dd42f854a49
carloscamacho <carlosalvarez9805@gmail.com>
2022-12-26 16:23:16 -0600

    create useContext

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,9 +1,29 @@
+import { useContext } from 'react'
+import { UserContext } from './context/UserContext';
 
 export const LoginPage = () => {
+
+    const { user, setUser } = useContext( UserContext );
+
     return (
         <>
             <h1>Login page</h1>
             <hr />
+
+            <pre>
+                { JSON.stringify( user, null, 3 ) }
+            </pre>
+
+            <button
+                className='btn btn-primary'
+                onClick={ () => setUser({
+                    id: 234,
+                    name: 'JoanArturo',
+                    email: 'joan.arturo_99@hotmail.com'
+                }) }
+            >
+                Establecer usuario
+            </button>
         </>
     )
 }
--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import { UserContext } from './UserContext'
 
-const user = {
+/* const user = {
     id: '123',
     email: 'carlos.antonio_98@gmail.com',
     name: 'carlosantonio'
-};
+}; */
 
 export const UserProvider = ({ children }) => {
-  return (
-    <UserContext.Provider value={{ hola: 'Mundo', user: user }}>
-        { children }
-    </UserContext.Provider>
-  );
+
+    const [user, setUser] = useState();
+
+    return (
+        //<UserContext.Provider value={{ hola: 'Mundo', user: user }}>
+        <UserContext.Provider value={{ user, setUser }}>
+            { children }
+        </UserContext.Provider>
+    );
 }
